c5e040d0874014fe8e415a8c4ce6a6aff15228ef
spcmd <spcmdgit@gmail.com>
2015-07-05 18:46:13 +0200

    added: vimphint, renamed: whint->wikihint

---
+++ vimphint.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#                                      _
+#         ___ _ __   ___ _ __ ___   __| |
+#        / __| '_ \ / __| '_ ` _ \ / _` |
+#        \__ | |_) | (__| | | | | | (_| |
+#        |___| .__/ \___|_| |_| |_|\__,_|
+#             |_|
+#
+#                  vimphint.sh 
+#               Created by: spcmd
+#           http://spcmd.github.io
+#           https://github.com/spcmd
+#           https://gist.github.com/spcmd
+#
+#
+#   vimphint.sh lists the custom commands
+#   and key maps from the .vimperatorrc
+#   and makes a simple html output of them
+
+help_file=/tmp/vimphint.html
+
+cat << 'EOF' > $help_file
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>vimphint.sh > html</title>
+  <style>
+    body { 
+        font-family:monospace; 
+        font-size:13px;
+    }
+    span.command { color:red; }
+    span.title { color:blue; font-weight:bold; }
+  </style>
+</head>
+<body>
+<p>
+<span class="title">
+vimphint - key maps:<br>
+======================
+</span>
+</p>
+EOF
+awk '/^nmap/' .vimperatorrc | awk '{$1=""; gsub("<", "\\&lt;", $2); gsub(">", "\\&gt;", $2); gsub("<", "\\&lt;", $NF); gsub(">", "\\&gt;", $NF); $2="<p><span class=\"command\">"$2"<\/span>"; print $0"</p>"}' >> $help_file
+cat << 'EOF' >> $help_file
+<p>
+<span class="title">
+vimphint - commands:<br>
+======================
+</span>
+</p>
+EOF
+ awk '/^command!/' .vimperatorrc | awk '{$1=""; $2="<p><span class=\"command\">"$2"<\/span>"; gsub("<", "\\&lt;", $NF); gsub(">", "\\&gt;", $NF); print $0"<\/p>"}' >> $help_file
+cat << 'EOF' >> $help_file && firefox $help_file
+</body>
+</html>
+EOF
