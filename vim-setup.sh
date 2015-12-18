c83b98c7d57b7f36a5be632bd2b160f150fb4282
spcmd <spcmdgit@gmail.com>
2015-12-18 16:33:49 +0100

    Deprecated

--- vim-setup.sh
+++ vim-setup.sh
@@ -13,31 +13,41 @@
 #           https://gist.github.com/spcmd
 #
 
+### This script is DEPRECATED!
+### Using vim-plug instead:
+### https://github.com/junegunn/vim-plug
+
+
+echo 'This script is DEPRECATED!'
+echo 'Using vim-plug instead:'
+echo 'https://github.com/junegunn/vim-plug'
+exit
+
 # Setting up the directories
-VIM_DIR=~/.vim
-VIM_AUTOLOAD_DIR=$VIM_DIR/autoload
-VIM_BUNDLE_DIR=$VIM_DIR/bundle
+#VIM_DIR=~/.vim
+#VIM_AUTOLOAD_DIR=$VIM_DIR/autoload
+#VIM_BUNDLE_DIR=$VIM_DIR/bundle
 
 # If .vim dir doesn't exist, create it
-if [[ ! -d $VIM_DIR ]]; then
-    mkdir -p $VIM_DIR
-fi
+#if [[ ! -d $VIM_DIR ]]; then
+    #mkdir -p $VIM_DIR
+#fi
 
 # Create the autoload and bunde dir inside .vim
-mkdir -p $VIM_AUTOLOAD_DIR $VIM_BUNDLE_DIR
+#mkdir -p $VIM_AUTOLOAD_DIR $VIM_BUNDLE_DIR
 
 # Change to the working directory
-cd $VIM_DIR
+#cd $VIM_DIR
 
 # Get Pathogen
-git clone https://github.com/tpope/vim-pathogen.git
-mv vim-pathogen/autoload/pathogen.vim $VIM_AUTOLOAD_DIR
-echo "pathogen.vim moved to: $VIM_AUTOLOAD_DIR"
-rm -rf vim-pathogen
-echo "vim-pathogen directory removed, not needed anymore."
+#git clone https://github.com/tpope/vim-pathogen.git
+#mv vim-pathogen/autoload/pathogen.vim $VIM_AUTOLOAD_DIR
+#echo "pathogen.vim moved to: $VIM_AUTOLOAD_DIR"
+#rm -rf vim-pathogen
+#echo "vim-pathogen directory removed, not needed anymore."
 
 # Change to the bundle dir
-cd $VIM_BUNDLE_DIR
+#cd $VIM_BUNDLE_DIR
 
 # Get the plugins 
 
@@ -61,15 +71,15 @@ git clone https://github.com/chrisbra/Colorizer.git
 git clone https://github.com/ap/vim-buftabline.git
 
 # Patched powerline fonts needed, but you might have it, so not need to install automatically
-echo "Powerline fonts aren't installed automatically. If you don't have it already installed, please visit: https://github.com/powerline/fonts and install it manually. Then select one of the installed powerline font type in the terminal settings, and also set this font type in your .vimrc and/or .gvimrc"
+#echo "Powerline fonts aren't installed automatically. If you don't have it already installed, please visit: https://github.com/powerline/fonts and install it manually. Then select one of the installed powerline font type in the terminal settings, and also set this font type in your .vimrc and/or .gvimrc"
 
 # Setup vim config files from dotfiles with stow
-if [[ -d ~/dotfiles ]]; then
-    cd ~/dotfiles
-    echo "Using stow to setup Vim config files from ~/dotfiles"
-    stow vim
-    echo "DONE: Vim config files has been set."
-fi
+#if [[ -d ~/dotfiles ]]; then
+    #cd ~/dotfiles
+    #echo "Using stow to setup Vim config files from ~/dotfiles"
+    #stow vim
+    #echo "DONE: Vim config files has been set."
+#fi
 
 # --- NOT USED ---
 
