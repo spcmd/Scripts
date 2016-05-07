e675ac861f786a79660ae8506df3b58f4f197d95
spcmd <spcmdgit@gmail.com>
2016-05-07 19:10:33 +0200

    added

---
+++ tmux-default-session.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#                               _
+#  ___ _ __   ___ _ __ ___   __| |
+# / __| '_ \ / __| '_ ` _ \ / _` |
+# \__ | |_) | (__| | | | | | (_| |
+# |___| .__/ \___|_| |_| |_|\__,_|
+#     |_|
+# Created by: spcmd
+# http://spcmd.github.io
+# https://github.com/spcmd
+
+# Start a default tmux session with predefined windows/programs
+
+tmux new-session -d -c $HOME
+tmux new-window -n files -c $HOME ranger
+tmux new-window -n torr -c $HOME rtorrent
+tmux new-window -n mus -c $HOME
+tmux new-window -n mail -c $HOME
+tmux new-window -n news -c $HOME newsbeuter
+tmux -2 attach-session -d
