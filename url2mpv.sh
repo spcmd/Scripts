3a982d027aa9af0347c6dd937767de866efa74c0
spcmd <spcmdgit@gmail.com>
2016-06-15 17:36:07 +0200

    quote fix

--- url2mpv.sh
+++ url2mpv.sh
@@ -22,7 +22,7 @@ if [ "$AUTO_STOP" = "yes" ]; then
 
     # mps-youtube
     mpsyt_dest=$(dbus-send --session --dest=org.freedesktop.DBus --type=method_call --print-reply /org/freedesktop/DBus org.freedesktop.DBus.ListNames | awk '/mps-youtube/{gsub("\042","");print $2}' | head -n1)
-    mpsyt_status=$(dbus-send --print-reply --session --dest=$mpsyt_dest /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus' | awk '{gsub("\042","");print $3}' | tail -n1)
+    mpsyt_status=$(dbus-send --print-reply --session --dest="$mpsyt_dest" /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus' | awk '{gsub("\042","");print $3}' | tail -n1)
 
     if [ "$mpsyt_status" = "Playing" ]; then
         # pause (don't stop, because 'Stop' really stops the whole playback and won't remember the track number and position)
