b628fc20d803fa13b0b51f78fc32e2840222e492
spcmd <spcmdgit@gmail.com>
2017-10-21 19:09:53 +0200

    handle different playlists

--- podcast_blackberry.sh
+++ podcast_blackberry.sh
@@ -11,20 +11,93 @@
 # Copy podcasts to the Blackberry podcast dir and append them to the playlist
 
 dir_podcasts=/media/BLACKBERRY/BlackBerry/podcasts
-file_playlist="$dir_podcasts/Podcasts.m3u"
 
-for file in "$@"; do
-
-    file_renamed=$(echo "$file" | sed 's/á/a/g;s/é/e/g;s/í/i/g;s/[óöő]/o/g;s/[üűú]/u/g;s/ /_/g')
-    filename_wo_ext="${file_renamed%.*}"
-
-    echo "Renaming $file to: $file_renamed"
-    mv "$file" "$file_renamed"
-    echo "Moving $file_renamed to Blackberry's podcasts dir..."
-    mv "$file_renamed" "$dir_podcasts"
-    echo "Appending $file_renamed to playlist..."
+# Different playlists
+append_to_playlist_SZFC() {
+    file_playlist="$dir_podcasts/SZFC.m3u"
+    echo "Appending $file_renamed to SZFC playlist..."
     echo "#EXTINF:-1,$filename_wo_ext" >> "$file_playlist"
     echo "file:///SDCard/BlackBerry/podcasts/$file_renamed" >> "$file_playlist"
-    echo "Done!"
+}
 
-done
+append_to_playlist_IHSZ() {
+    file_playlist="$dir_podcasts/IHSZ.m3u"
+    echo "Appending $file_renamed to IHSZ playlist..."
+    echo "#EXTINF:-1,$filename_wo_ext" >> "$file_playlist"
+    echo "file:///SDCard/BlackBerry/podcasts/$file_renamed" >> "$file_playlist"
+}
+
+append_to_playlist_DPC() {
+    file_playlist="$dir_podcasts/DPC.m3u"
+    echo "Appending $file_renamed to DPC playlist..."
+    echo "#EXTINF:-1,$filename_wo_ext" >> "$file_playlist"
+    echo "file:///SDCard/BlackBerry/podcasts/$file_renamed" >> "$file_playlist"
+}
+
+append_to_playlist_VEGYES() {
+    file_playlist="$dir_podcasts/VEGYES.m3u"
+    echo "Appending $file_renamed to VEGYES playlist..."
+    echo "#EXTINF:-1,$filename_wo_ext" >> "$file_playlist"
+    echo "file:///SDCard/BlackBerry/podcasts/$file_renamed" >> "$file_playlist"
+}
+show_help() {
+    echo "Playlist names: szfc, dpc, ihsz, vegyes"
+}
+
+case $1 in
+    ihsz)
+        for file in "${@:2}"; do
+            file_renamed=$(echo "$file" | sed 's/á/a/g;s/é/e/g;s/í/i/g;s/[óöő]/o/g;s/[üűú]/u/g;s/ /_/g')
+            filename_wo_ext="${file_renamed%.*}"
+            echo "Renaming $file to: $file_renamed"
+            mv "$file" "$file_renamed"
+            echo "Moving $file_renamed to Blackberry's podcasts dir..."
+            mv "$file_renamed" "$dir_podcasts"
+            append_to_playlist_IHSZ
+            echo "Done!"
+        done
+        ;;
+    szfc)
+        for file in ${@:2}; do
+            file_renamed=$(echo "$file" | sed 's/á/a/g;s/é/e/g;s/í/i/g;s/[óöő]/o/g;s/[üűú]/u/g;s/ /_/g')
+            filename_wo_ext="${file_renamed%.*}"
+            echo "Renaming $file to: $file_renamed"
+            mv "$file" "$file_renamed"
+            echo "Moving $file_renamed to Blackberry's podcasts dir..."
+            mv "$file_renamed" "$dir_podcasts"
+            append_to_playlist_SZFC
+            echo "Done!"
+        done
+        ;;
+    dpc)
+        for file in ${@:2}; do
+            file_renamed=$(echo "$file" | sed 's/á/a/g;s/é/e/g;s/í/i/g;s/[óöő]/o/g;s/[üűú]/u/g;s/ /_/g')
+            filename_wo_ext="${file_renamed%.*}"
+            echo "Renaming $file to: $file_renamed"
+            mv "$file" "$file_renamed"
+            echo "Moving $file_renamed to Blackberry's podcasts dir..."
+            mv "$file_renamed" "$dir_podcasts"
+            append_to_playlist_DPC
+            echo "Done!"
+        done
+        ;;
+    vegyes)
+        for file in "${@:2}"; do
+            file_renamed=$(echo "$file" | sed 's/á/a/g;s/é/e/g;s/í/i/g;s/[óöő]/o/g;s/[üűú]/u/g;s/ /_/g')
+            filename_wo_ext="${file_renamed%.*}"
+            echo "Renaming $file to: $file_renamed"
+            mv "$file" "$file_renamed"
+            echo "Moving $file_renamed to Blackberry's podcasts dir..."
+            mv "$file_renamed" "$dir_podcasts"
+            append_to_playlist_VEGYES
+            echo "Done!"
+        done
+        ;;
+      -h|--help)
+          show_help
+          ;;
+      ""|*)
+          echo "Error! It's not a valid playlist name!"
+          show_help
+        ;;
+esac
